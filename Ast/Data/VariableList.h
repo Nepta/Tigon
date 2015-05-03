6cff39fb68906af193215765e5666d3db5eed05b
nepta <nepta23@gmail.com>
2015-05-03 04:25:51 +0200

    variable are expression

--- Ast/Data/VariableList.h
+++ Ast/Data/VariableList.h
@@ -2,27 +2,18 @@
 #define __Variable_H__
 
 #include "Visitable.h"
-#include "Constante.h"
 #include <unordered_map>
-//#include "../Control/Interpreter.h"
 
 class VariableList{
-	std::unordered_map<std::string, Constante*> variableList_;
+	std::unordered_map<std::string, Visitable*> variableList_;
 
 public:
-	Constante* getValue(std::string name){
+	Visitable* getValue(std::string name){
 		return variableList_[name];
 	}
 	
-	void addValue(std::string name, Constante *value){
+	void addValue(std::string name, Visitable *value){
 		variableList_[name] = value;
-	}
-	
-	void addValue(std::string name, Visitable *v){
-//		Interpreter i(*v);
-//		int value = i.peakVar();
-//		Constante* interpretedValue = new Int(value);
-//		variableList_[name] = interpretedValue;
 	}
 };
 
